24bb97b67ef0def4bec4b79fe00aea3e8cb8f0fa
WhiteeST <whiteeluck123@gmail.com>
2022-08-27 15:15:15 +0400

    feat: codejam done

--- src/app.js
+++ src/app.js
@@ -23,20 +23,20 @@ let difficultyMode;
 let requiredAllStagesCards;
 let playDeck;
 let resultDeck = [];
-let nextCard = 1;
+let nextCard;
 let colorTracker = {};
 
 const easyBlueCards = ['blue3', 'blue4', 'blue5', 'blue10'];
 const normalBlueCards = ['blue7', 'blue9', 'blue11', 'blue12'];
-const hardlBlueCards = ['blue1', 'blue2', 'blue6', 'blue8'];
+const hardBlueCards = ['blue1', 'blue2', 'blue6', 'blue8'];
 
 const easyGreenCards = ['green1', 'green12', 'green16', 'green17', 'green18'];
 const normalGreenCards = ['green7', 'green8', 'green9', 'green10', 'green11', 'green13', 'green14', 'green15'];
-const hardlGreenCards = ['green2', 'green3', 'green4', 'green5', 'green6'];
+const hardGreenCards = ['green2', 'green3', 'green4', 'green5', 'green6'];
 
 const easyBrownCards = ['brown11', 'brown12', 'brown13', 'brown14', 'brown21'];
 const normalBrownCards = ['brown1', 'brown2', 'brown3', 'brown4', 'brown5', 'brown15', 'brown16', 'brown17', 'brown18', 'brown19', 'brown20'];
-const hardlBrownCards = ['brown6', 'brown7', 'brown8', 'brown9', 'brown10'];
+const hardBrownCards = ['brown6', 'brown7', 'brown8', 'brown9', 'brown10'];
 
 const statusTrackerStageHandler = (stage) => {
   let index = 0;
@@ -57,29 +57,66 @@ const statusTrackerHandler = () => {
   statusTrackerStageHandler('thirdStage');
 }
 
-const createEasyTempColorDeck = (color) => {
+const createTempColorDeck = (color, difficulty) => {
   let tempDeck = [];
   const colorCounter = requiredAllStagesCards[color === 'green' ? 0 : color === 'brown' ? 1 : 2];
   let easyColorCard;
   let normalColorCard;
+  let hardColorCard;
   let sourceCards;
+  let fullDifficultydeck;
   switch (color) {
-    case 'green': easyColorCard = easyGreenCards.slice(0); normalColorCard = normalGreenCards.slice(0); sourceCards = Object.assign({}, greenCards); break;
-    case 'brown': easyColorCard = easyBrownCards.slice(0); normalColorCard = normalBrownCards.slice(0); sourceCards = Object.assign({}, brownCards); break;
-    case 'blue': easyColorCard = easyBlueCards.slice(0); normalColorCard = normalBlueCards.slice(0); sourceCards = Object.assign({}, blueCards); break;
+    case 'green': easyColorCard = easyGreenCards.slice(0); normalColorCard = normalGreenCards.slice(0); sourceCards = Object.assign({}, greenCards); hardColorCard = hardGreenCards.slice(0); break;
+    case 'brown': easyColorCard = easyBrownCards.slice(0); normalColorCard = normalBrownCards.slice(0); sourceCards = Object.assign({}, brownCards); hardColorCard = hardBrownCards.slice(0); break;
+    case 'blue': easyColorCard = easyBlueCards.slice(0); normalColorCard = normalBlueCards.slice(0); sourceCards = Object.assign({}, blueCards); hardColorCard = hardBlueCards.slice(0); break;
   }
-  easyColorCard.forEach(card => {
-    if (tempDeck.length < colorCounter)
-      tempDeck.push(sourceCards[card])
-  })
-  if (tempDeck.length < colorCounter) {
-    while (tempDeck.length < colorCounter) {
-      const cardId = Math.floor(Math.random() * normalColorCard.length);
-      tempDeck.push(sourceCards[normalColorCard[cardId]]);
-      normalColorCard[cardId] = normalColorCard[normalColorCard.length - 1];
-      normalColorCard.pop();
+
+  const mixDifficultyDeck = (deckArray) => {
+    if (tempDeck.length < colorCounter) {
+      while (tempDeck.length < colorCounter) {
+        const cardId = Math.floor(Math.random() * deckArray.length);
+        tempDeck.push(sourceCards[deckArray[cardId]]);
+        deckArray[cardId] = deckArray[deckArray.length - 1];
+        deckArray.pop();
+      }
     }
   }
+  switch (difficulty) {
+    case 'Очень низкая':
+      {
+        easyColorCard.forEach(card => {
+          if (tempDeck.length < colorCounter)
+            tempDeck.push(sourceCards[card])
+        })
+        fullDifficultydeck = normalColorCard;
+        break;
+      }
+    case 'Низкая':
+      {
+        fullDifficultydeck = ([...easyColorCard] + ',' + [...normalColorCard]).split(',');
+        break;
+      }
+    case 'Средняя':
+      {
+        fullDifficultydeck = ([...easyColorCard] + ',' + [...normalColorCard] + ',' + [...hardColorCard]).split(',');
+        break;
+      }
+    case 'Высокая':
+      {
+        fullDifficultydeck = ([...normalColorCard] + ',' + [...hardColorCard]).split(',');
+        break;
+      }
+    case 'Очень высокая':
+      {
+        hardColorCard.forEach(card => {
+          if (tempDeck.length < colorCounter)
+            tempDeck.push(sourceCards[card])
+        })
+        fullDifficultydeck = normalColorCard;
+        break;
+      }
+  }
+  mixDifficultyDeck(fullDifficultydeck);
   return tempDeck;
 }
 
@@ -106,6 +143,7 @@ const extractFromArray = (array) => {
 }
 
 const createDeck = (difficultyMode) => {
+  console.log(difficultyMode)
   //узнаем кол-во необходимых карт для колоды
   resultDeck = [];
   requiredAllStagesCards = [
@@ -113,54 +151,41 @@ const createDeck = (difficultyMode) => {
     parseInt(cardReserve[1].textContent) + parseInt(cardReserve[4].textContent) + parseInt(cardReserve[7].textContent), //коричневые
     parseInt(cardReserve[2].textContent) + parseInt(cardReserve[5].textContent) + parseInt(cardReserve[8].textContent)  //синие
   ];
-  if (difficultyMode === 'Очень низкая') {
-    //берем все легкие + обычные, если не хватает и кладем их в tempDeck
-    playDeck = [(createEasyTempColorDeck('green')), createEasyTempColorDeck('brown'), createEasyTempColorDeck('blue')];
 
-    playDeck.forEach((deck, index) => {
-      deck.forEach(item => {
-        colorTracker[item] = index === 0 ? 'green' : index === 1 ? 'brown' : 'blue';
-      })
+  //берем все легкие + обычные, если не хватает и кладем их в tempDeck
+  playDeck = [(createTempColorDeck('green', difficultyMode)), createTempColorDeck('brown', difficultyMode), createTempColorDeck('blue', difficultyMode)];
+
+  playDeck.forEach((deck, index) => {
+    deck.forEach(item => {
+      colorTracker[item] = index === 0 ? 'green' : index === 1 ? 'brown' : 'blue';
     })
+  })
 
-    // let b = playDeck.map(element => {
-    //   return element.slice(0);
-    // });
-    // console.log('playDeck', b)
+  let stageOneDeck = [createSortedStageDeck('firstStage', 'greenCards'), createSortedStageDeck('firstStage', 'brownCards'), createSortedStageDeck('firstStage', 'blueCards')];
+  let stageTwoDeck = [createSortedStageDeck('secondStage', 'greenCards'), createSortedStageDeck('secondStage', 'brownCards'), createSortedStageDeck('secondStage', 'blueCards')];
+  let stageThreeDeck = [createSortedStageDeck('thirdStage', 'greenCards'), createSortedStageDeck('thirdStage', 'brownCards'), createSortedStageDeck('thirdStage', 'blueCards')];
 
+  stageOneDeck = extractFromArray(stageOneDeck);
+  stageTwoDeck = extractFromArray(stageTwoDeck);
+  stageThreeDeck = extractFromArray(stageThreeDeck);
 
-    let stageOneDeck = [createSortedStageDeck('firstStage', 'greenCards'), createSortedStageDeck('firstStage', 'brownCards'), createSortedStageDeck('firstStage', 'blueCards')];
-    let stageTwoDeck = [createSortedStageDeck('secondStage', 'greenCards'), createSortedStageDeck('secondStage', 'brownCards'), createSortedStageDeck('secondStage', 'blueCards')];
-    let stageThreeDeck = [createSortedStageDeck('thirdStage', 'greenCards'), createSortedStageDeck('thirdStage', 'brownCards'), createSortedStageDeck('thirdStage', 'blueCards')];
+  const tempDeckArray = [stageOneDeck, stageTwoDeck, stageThreeDeck];
 
-    stageOneDeck = extractFromArray(stageOneDeck);
-    stageTwoDeck = extractFromArray(stageTwoDeck);
-    stageThreeDeck = extractFromArray(stageThreeDeck);
-
-    const tempDeckArray = [stageOneDeck, stageTwoDeck, stageThreeDeck];
-
-
-    // let a = tempDeckArray.map(element => {
-    //   return element.slice(0);
-    // });
-    // console.log('tempDeckArray', a)
-
-    tempDeckArray.forEach(deck => {
-      while (deck.length) {
-        const cardId = Math.floor(Math.random() * deck.length);
-        if (deck[cardId] != "")
-          resultDeck.push(deck[cardId]);
-        deck[cardId] = deck[deck.length - 1];
-        deck.pop();
-      }
-    })
-  }
+  tempDeckArray.forEach(deck => {
+    while (deck.length) {
+      const cardId = Math.floor(Math.random() * deck.length);
+      if (deck[cardId] != "")
+        resultDeck.push(deck[cardId]);
+      deck[cardId] = deck[deck.length - 1];
+      deck.pop();
+    }
+  })
 }
 
 difficultyButton.forEach(difficulty => {
   difficulty.addEventListener('click', (event) => {
     difficultyButton.forEach(button => {
-      button === event.target ? button.classList.add('picked') : button.classList.remove('picked');
+      button === event.target ? button.classList.add('picked-black') : button.classList.remove('picked-black');
     })
     difficultyMode = difficulty.textContent;
     // console.log(currentAncient);
@@ -181,25 +206,29 @@ ancientsData.forEach(ancient => {
 
 ancientsArray.forEach((ancient, index) => {
   ancient.addEventListener('click', () => {
-    ancientsArray.forEach(element => element.classList = 'ancient-card');
-    ancient.classList.add('picked');
-    currentAncient = ancientsData[index].name;
-    currentAncientId = index;
-    if (currentAncient) {
-      statusTrackerHandler();
+    if (nextCard === undefined || nextCard === resultDeck.length || nextCard === 0) {
+      ancientsArray.forEach(element => element.classList = 'ancient-card');//убираем picked у всех остальных
+      ancient.classList.add('picked');
+      completeDeck.classList.add('invisible');
+      completeDeck.classList.remove('rotate');
+      currentAncient = ancientsData[index].name;
+      currentAncientId = index;
+      if (currentAncient) {
+        statusTrackerHandler();
+      }
     }
   })
 })
 
 createDeckButton.addEventListener('click', () => {
-  nextCard = 0;
-  createDeckButton.classList.add('picked');
-  currentCard.classList.add('invisible');
-  completeDeck.classList.remove('invisible');
-  if (currentAncient) {
+  if (currentAncient && difficultyMode) {
+    nextCard = 0;
+    createDeckButton.classList.add('picked-black');
+    currentCard.classList.add('invisible');
+    completeDeck.classList.remove('invisible');
+    completeDeck.classList.add('rotate')
     statusTrackerHandler();
     createDeck(difficultyMode);
-
   }
 })
 
@@ -215,7 +244,7 @@ const changeTrackerStatus = (color) => {
 
 completeDeck.addEventListener('click', () => {
   if (nextCard === 0) {
-    createDeckButton.classList.remove('picked');
+    createDeckButton.classList.remove('picked-black');
     currentCard.classList.remove('invisible');
     currentCard.style.backgroundImage = `url('${resultDeck[nextCard]}')`;
     changeTrackerStatus(colorTracker[resultDeck[nextCard]]);
@@ -225,12 +254,19 @@ completeDeck.addEventListener('click', () => {
 
 currentCard.addEventListener('click', () => {
   if (nextCard < resultDeck.length) {
-    currentCard.style.backgroundImage = `url('${resultDeck[nextCard]}')`;
-    changeTrackerStatus(colorTracker[resultDeck[nextCard]]);
-    nextCard++;
+    const img = new Image();
+    const imgSource = `${resultDeck[nextCard]}`;
+    img.src = imgSource;
+    img.onload = () => {
+      currentCard.style.backgroundImage = `url('${resultDeck[nextCard]}')`;
+      changeTrackerStatus(colorTracker[resultDeck[nextCard]]);
+      nextCard++;
+    };
   }
   else {
     currentCard.style.backgroundImage = null;
-    currentCard.classList.add('invisible')
+    currentCard.classList.add('invisible');
+    completeDeck.classList.add('invisible');
+    completeDeck.classList.remove('rotate');
   }
 })
